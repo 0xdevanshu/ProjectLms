18c8295ad0db3442b8a4640e125640f8217d60ba
0xdevanshu <devanshu.it@gmail.com>
2020-05-04 13:01:01 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Title -->
+    <title>FLMS | Home Page</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta charset="UTF-8">
+    <meta name="description" content="Responsive Admin Dashboard Template" />
+    <meta name="keywords" content="admin,dashboard" />
+    <meta name="author" content="Steelcoders" />
+
+    <!-- Styles -->
+    <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css" />
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">
+
+
+    <!-- Theme Styles -->
+    <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="http://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="http://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+<body>
+    <div class="loader-bg"></div>
+    <div class="loader">
+        <div class="preloader-wrapper big active">
+            <div class="spinner-layer spinner-blue">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+            <div class="spinner-layer spinner-spinner-deep-purple lighten-1">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+            <div class="spinner-layer spinner-yellow">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+            <div class="spinner-layer spinner-green">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="gap-patch">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mn-content fixed-sidebar view intro-2">
+        <header class="mn-header navbar-fixed">
+            <nav class="blue accent-1 fixed-top navbar-expand-lg scrolling-navbar">
+                <div class="nav-wrapper row">
+                    <!-- <section class="material-design-hamburger navigation-toggle hide-on-med-and-up">
+                        <a href="#" data-activates="slide-out" class="button-collapse show-on-large material-design-hamburger__icon">
+                            <span class="material-design-hamburger__layer"></span>
+                        </a>
+                    </section> -->
+                    <span href="#!" class="brand-logo chapter-title"><i class="material-icons">cloud</i>FLMS</span>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="badges.html">About Us</a></li>
+                        <li><a class="waves-effect waves-light btn" id="modal-trigger" data-buttonTitle="Log in">Log in</a></li>
+                    </ul>
+                    </form>
+
+
+                </div>
+            </nav>
+        </header>
+
+
+        
+        <div class="row main-content">
+            <div class="col s12 m5 left-con ">
+                <h1> <span style="color: white">FLMS</span>| Faculty Leave Management System</h1>
+                <h6> Simplifying lives of our respeceted faculties.</h6>
+            </div>
+            <div class="col s12 m7 right-con">
+                <div id="modal" class="hide">
+                    <div id="content">
+                        <a class="icons" href="faculty/"><i class="large material-icons">assignment_ind</i><span>Faculty</span></a>
+                        <a class="icons" href="department/"><i class="large material-icons">account_circle</i><span>Head of Department</span></a>
+                        <a class="icons" href="admin/"><i class="large material-icons">verified_user</i><span>Principal</span></a>
+                    </div>
+                    <div id="background">sdsdsd</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="left-sidebar-hover"></div>
+    <!-- Javascripts -->
+    <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+    <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+    <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+    <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/alpha.min.js"></script>
+
+</body>
+
+</html>
